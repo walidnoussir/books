1ae23b2426f8fb14b223e5b877466c04e6da99a5
ali1 <aliouaneoussama89@gmail.com>
2025-03-20 16:19:13 +0000

    ''

--- client/src/features/login/Login.jsx
+++ client/src/features/login/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <form>
       <h1>Welcome back</h1>
-      <input type="text" className="bg-gray-50 p-3  text-gray-900 rounded-lg" />
+      <input type="text" className="input" />
     </form>
   );
 };
