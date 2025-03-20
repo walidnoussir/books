4b70eb087edc051dcdb4fa19dcb194252b5eafc7
walid <noussirw@gmail.com>
2025-03-20 16:41:52 +0000

    modify sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,12 @@
+import Form from "../ui/Form";
+
 function SignUp() {
-  return <div></div>;
+  return (
+    <div className="py-3 px-5">
+      <h1 className="text-primary-900 font-bold text-2xl">Sign Up</h1>
+      <Form />
+    </div>
+  );
 }
 
 export default SignUp;
--- client/src/ui/Form.jsx
+++ client/src/ui/Form.jsx
@@ -2,11 +2,19 @@ import Input from "../ui/Input";
 
 function Form() {
   return (
-    <div>
+    <form>
       <Input label="name">
         <input className="input" type="text" placeholder="Your name" />
       </Input>
-    </div>
+
+      <Input label="email">
+        <input className="input" type="text" placeholder="Your name" />
+      </Input>
+
+      <Input label="password">
+        <input className="input" type="text" placeholder="Your name" />
+      </Input>
+    </form>
   );
 }
 
